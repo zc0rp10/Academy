7040f4758c89fe6281ae0b7266a0a4a5f05537ed
Björn <55206983+zc0rp10@users.noreply.github.com>
2020-12-23 12:18:39 +0100

    Lesson #1

---
+++ Hangman/Hangman/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartGame();
+            PlayGame();
+            EndGame();
+        }
+
+        private static void StartGame()
+        {
+            Console.WriteLine("Start Game");
+            AskForUsersName();
+        }
+
+        static void AskForUsersName()
+        {
+            Console.WriteLine("Asking user for name...");
+        }
+
+        private static void PlayGame()
+        {
+            Console.WriteLine("Play Game");
+            DisplayMaskedWord();
+            AskForLetter();
+        }
+
+        static void DisplayMaskedWord()
+        {
+            Console.WriteLine("Display masked word");
+        }
+        
+        static void AskForLetter()
+        {
+            Console.WriteLine("Asking for letter");
+        }
+
+        private static void EndGame()
+        {
+            Console.WriteLine("Game Over");
+
+        }
+    }
+}
