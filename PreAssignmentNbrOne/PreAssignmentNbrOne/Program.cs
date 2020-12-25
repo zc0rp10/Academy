1a0e4d77c258f273ab26ef2bb0f4a00b2def1e9e
Björn <55206983+zc0rp10@users.noreply.github.com>
2020-12-25 19:31:15 +0100

    Inlämningsuppgift #1 förstudier

---
+++ PreAssignmentNbrOne/PreAssignmentNbrOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PreAssignmentNbrOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bo";
+            int age = 30;
+        }
+
+        private static bool PrintOut(string name, int age)
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}");
+
+            if (age > 25)
+                return true;
+            else
+                return false;
+        }
+    }
+}
