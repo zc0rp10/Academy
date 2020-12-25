a59d035a5661483a31ad881110ed185d844339db
Björn <55206983+zc0rp10@users.noreply.github.com>
2020-12-25 15:28:46 +0100

    Added Classes and Objects example

---
+++ Examples/ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Bo", 30);
+            Person person2 = new Person("Li", 40);
+
+            person1.Age = 30;
+            person2.Age = 40;
+
+            Console.WriteLine(person1.Name);
+            Console.WriteLine(person1.Age);
+        }
+    }
+}
