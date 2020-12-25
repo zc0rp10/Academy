34a44a75ec293ecd8ca0d374c214122177808271
Björn <55206983+zc0rp10@users.noreply.github.com>
2020-12-25 16:31:27 +0100

    Added Properties examples

---
+++ Examples/Properties/Properties/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties
+{
+    class Person
+    {
+        private int age; //propfull Full Property, use when you NEED to validate on get or set.
+        
+        public int Age
+        {
+            get { return age; }
+            set {
+                if (value < 0)
+                    age = 0;
+                else
+                    age = value; }
+        }
+
+        public string Name { get; private set; } //prop Automatic Proerty, use when you DON'T need to validate assignments.
+                                                 //You can add private to set if you want the prop to only be set up from constructor, and not changed later.
+
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+}
---
+++ Examples/Properties/Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(30, "Sture");
+
+            person.Age = 5;
+            person.Age = -10;
+
+            Console.WriteLine(person.Age);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
