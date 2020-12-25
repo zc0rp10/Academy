450665d629caf541b5a7916b61249d62dfcdb43d
Björn <55206983+zc0rp10@users.noreply.github.com>
2020-12-25 16:55:32 +0100

    Added File Managment example

---
+++ Examples/FileManagment/FileManagment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileManagment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\zcorp\Desktop\MyFile.txt";
+            string textString = $"Line 1{Environment.NewLine}Line 2\nLine 3";
+
+            File.WriteAllText(filePath, textString);
+
+            //As a single string
+            Console.WriteLine(File.ReadAllText(filePath));
+
+            //As a string array, each line is one array entry.
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine(lines.Length);
+        }
+    }
+}
