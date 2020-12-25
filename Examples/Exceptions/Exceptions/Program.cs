03b94edb54eb08936b87fac9c8204f379da85002
Björn <55206983+zc0rp10@users.noreply.github.com>
2020-12-25 18:46:03 +0100

    Added Exception Handling example

---
+++ Examples/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Divide(10, 0));
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid Argument...");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Oops, something went wrong...");
+            }
+        }
+
+        static int Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new ArgumentException("Argument must not be zero.", nameof(b));
+            else if
+                (a == 0)
+                throw new ArgumentException("Argument must not be zero.", nameof(a));
+
+            return a / b;
+        }
+    }
+}
