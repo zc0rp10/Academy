507697dbc57ed72dac6e4a23eafce5db9e893b44
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-06 12:58:07 +0100

    CSharpSkolan Intro Klasser

---
+++ CSharpSkolan/KlasserEnIntroduktion/KlasserEnIntroduktion/InkomstInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KlasserEnIntroduktion
+{
+    class InkomstInfo
+    {
+        public string Namn { get; set; }
+        public int Inkomst { get; set; }
+        public int Timmar { get; set; }
+
+        public int Timlön { get { return Inkomst / Timmar; } }
+    }
+}
---
+++ CSharpSkolan/KlasserEnIntroduktion/KlasserEnIntroduktion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KlasserEnIntroduktion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InkomstInfo info = new InkomstInfo();
+            Console.WriteLine("Ange ditt namn: ");
+            info.Namn = Console.ReadLine();
+            Console.WriteLine("Ange din inkomst: ");
+            info.Inkomst = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ange antal timmar: ");
+            info.Timmar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(info.Namn + ", din timlön blev: " + info.Timlön + " kr/h.");
+        }
+    }
+}
