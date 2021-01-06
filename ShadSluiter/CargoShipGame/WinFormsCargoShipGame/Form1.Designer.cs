57dd1a682f519c1ee5b0a5898e0df1cd9fd5d778
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-06 22:31:20 +0100

    Added Cargo Ship Game Console Version

---
+++ ShadSluiter/CargoShipGame/WinFormsCargoShipGame/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace WinFormsCargoShipGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.track_motorCycles = new System.Windows.Forms.TrackBar();
+            this.track_cars = new System.Windows.Forms.TrackBar();
+            this.track_trucks = new System.Windows.Forms.TrackBar();
+            this.btn_newShip = new System.Windows.Forms.Button();
+            this.track_trainCars = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_truckCount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label_trainCarCount = new System.Windows.Forms.Label();
+            this.label_cycleCount = new System.Windows.Forms.Label();
+            this.label_carCount = new System.Windows.Forms.Label();
+            this.label_shipLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_motorCycles)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_cars)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_trucks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_trainCars)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 330);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(173, 77);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(550, 149);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // track_motorCycles
+            // 
+            this.track_motorCycles.Location = new System.Drawing.Point(12, 366);
+            this.track_motorCycles.Name = "track_motorCycles";
+            this.track_motorCycles.Size = new System.Drawing.Size(387, 45);
+            this.track_motorCycles.TabIndex = 2;
+            this.track_motorCycles.Scroll += new System.EventHandler(this.track_motorCycles_Scroll);
+            // 
+            // track_cars
+            // 
+            this.track_cars.Location = new System.Drawing.Point(401, 366);
+            this.track_cars.Name = "track_cars";
+            this.track_cars.Size = new System.Drawing.Size(387, 45);
+            this.track_cars.TabIndex = 3;
+            this.track_cars.Scroll += new System.EventHandler(this.track_cars_Scroll);
+            // 
+            // track_trucks
+            // 
+            this.track_trucks.Location = new System.Drawing.Point(12, 464);
+            this.track_trucks.Name = "track_trucks";
+            this.track_trucks.Size = new System.Drawing.Size(387, 45);
+            this.track_trucks.TabIndex = 4;
+            this.track_trucks.Scroll += new System.EventHandler(this.track_trucks_Scroll);
+            // 
+            // btn_newShip
+            // 
+            this.btn_newShip.Location = new System.Drawing.Point(12, 518);
+            this.btn_newShip.Name = "btn_newShip";
+            this.btn_newShip.Size = new System.Drawing.Size(121, 42);
+            this.btn_newShip.TabIndex = 5;
+            this.btn_newShip.Text = "New Ship";
+            this.btn_newShip.UseVisualStyleBackColor = true;
+            this.btn_newShip.Click += new System.EventHandler(this.btn_newShip_Click);
+            // 
+            // track_trainCars
+            // 
+            this.track_trainCars.Location = new System.Drawing.Point(401, 464);
+            this.track_trainCars.Name = "track_trainCars";
+            this.track_trainCars.Size = new System.Drawing.Size(387, 45);
+            this.track_trainCars.TabIndex = 6;
+            this.track_trainCars.Scroll += new System.EventHandler(this.track_trainCars_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 494);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Trucks (11 Units)";
+            // 
+            // label_truckCount
+            // 
+            this.label_truckCount.AutoSize = true;
+            this.label_truckCount.Location = new System.Drawing.Point(198, 446);
+            this.label_truckCount.Name = "label_truckCount";
+            this.label_truckCount.Size = new System.Drawing.Size(13, 15);
+            this.label_truckCount.TabIndex = 8;
+            this.label_truckCount.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 494);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Train Cars (17 Units)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(146, 396);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Motorcycles (3 Units)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(557, 396);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Cars (5 Units)";
+            // 
+            // label_trainCarCount
+            // 
+            this.label_trainCarCount.AutoSize = true;
+            this.label_trainCarCount.Location = new System.Drawing.Point(589, 446);
+            this.label_trainCarCount.Name = "label_trainCarCount";
+            this.label_trainCarCount.Size = new System.Drawing.Size(13, 15);
+            this.label_trainCarCount.TabIndex = 13;
+            this.label_trainCarCount.Text = "0";
+            // 
+            // label_cycleCount
+            // 
+            this.label_cycleCount.AutoSize = true;
+            this.label_cycleCount.Location = new System.Drawing.Point(198, 348);
+            this.label_cycleCount.Name = "label_cycleCount";
+            this.label_cycleCount.Size = new System.Drawing.Size(13, 15);
+            this.label_cycleCount.TabIndex = 14;
+            this.label_cycleCount.Text = "0";
+            // 
+            // label_carCount
+            // 
+            this.label_carCount.AutoSize = true;
+            this.label_carCount.Location = new System.Drawing.Point(589, 348);
+            this.label_carCount.Name = "label_carCount";
+            this.label_carCount.Size = new System.Drawing.Size(13, 15);
+            this.label_carCount.TabIndex = 15;
+            this.label_carCount.Text = "0";
+            // 
+            // label_shipLabel
+            // 
+            this.label_shipLabel.AutoSize = true;
+            this.label_shipLabel.Location = new System.Drawing.Point(56, 235);
+            this.label_shipLabel.Name = "label_shipLabel";
+            this.label_shipLabel.Size = new System.Drawing.Size(53, 15);
+            this.label_shipLabel.TabIndex = 16;
+            this.label_shipLabel.Text = "Capacity";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 568);
+            this.Controls.Add(this.label_shipLabel);
+            this.Controls.Add(this.label_carCount);
+            this.Controls.Add(this.label_cycleCount);
+            this.Controls.Add(this.label_trainCarCount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label_truckCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.track_trainCars);
+            this.Controls.Add(this.btn_newShip);
+            this.Controls.Add(this.track_trucks);
+            this.Controls.Add(this.track_cars);
+            this.Controls.Add(this.track_motorCycles);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Cargo Ship Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_motorCycles)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_cars)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_trucks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.track_trainCars)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar track_motorCycles;
+        private System.Windows.Forms.TrackBar track_cars;
+        private System.Windows.Forms.TrackBar track_trucks;
+        private System.Windows.Forms.Button btn_newShip;
+        private System.Windows.Forms.TrackBar track_trainCars;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_truckCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label_trainCarCount;
+        private System.Windows.Forms.Label label_cycleCount;
+        private System.Windows.Forms.Label label_carCount;
+        private System.Windows.Forms.Label label_shipLabel;
+    }
+}
+
