b9b6018f68db5d08a1e522e1cc1d78a30be84749
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-12 12:10:07 +0100

    CSharpSkolan Nested Loops

---
+++ CSharpSkolan/NestedLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NestedLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    char x = (i % 2 == 0 && j% 2 == 0 || i % 2 != 0 && j % 2 != 0) ? 'o' : '#';
+                    Console.Write(x);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
