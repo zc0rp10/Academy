51d0426c5626057f663fbc1b67f11102a6f5d76a
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-12 12:09:21 +0100

    Validate Input (Num) example added

---
+++ CSharpSkolan/Inmatning/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inmatning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetValidNum();
+            Console.WriteLine(number);
+        }
+
+        private static int GetValidNum()
+        {
+            bool isValid;
+            int number;
+            string msg = "Enter a number: ";
+
+            do
+            {
+                Console.Write(msg);
+                msg = "Not an integer, please try again: ";
+                string input = Console.ReadLine();
+                isValid = int.TryParse(input, out number);
+            } while (!isValid);
+
+            return number;
+        }
+    }
+}
