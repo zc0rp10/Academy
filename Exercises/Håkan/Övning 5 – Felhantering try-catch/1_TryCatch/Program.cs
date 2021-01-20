49910d8f80c124cf014e9ccf3d9d98e96701abd9
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-20 16:18:44 +0100

    Dag 3 - Övning 5

---
+++ Exercises/Håkan/Övning 5 – Felhantering try-catch/1_TryCatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _1_TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapa en Console Application som hanterar fel inmatning i förra övningen, Ovning4.pd
+            EttTillHundra();
+        }
+
+        private static void EttTillHundra()
+        {
+            try
+            {
+                Console.Write("Skriv in ett heltal mellan 1 och 100: ");
+                int num = int.Parse(Console.ReadLine());
+                if (num > 0 && num <= 100)
+                {
+                    while (num < 101)
+                    {
+                        Console.WriteLine(num++);
+                    }
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR: Maxvärde på talet överskridet.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("ERROR: Felaktigt format på det inmata värdet. Sannorlikt angav du inget värde " +
+                    "eller så var värdet inte ett heltal.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: " + e.Message);
+            }
+        }
+    }
+}
