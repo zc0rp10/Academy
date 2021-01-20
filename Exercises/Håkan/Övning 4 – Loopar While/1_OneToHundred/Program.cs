d0af071452590745276ca5bdf7bbaf31522b36ea
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-20 13:57:22 +0100

    Dag 3 - Övning 4

---
+++ Exercises/Håkan/Övning 4 – Loopar While/1_OneToHundred/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1_OneToTwenty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapa en Console Application som med hjälp av en while loop skriver ut alla tal mellan 1 och 20
+            EttTillTjugo();
+            //EttTillTjugoExtra();
+
+        }
+
+        private static void EttTillTjugo()
+        {
+            int i = 1;
+            while (i <= 20)
+            {
+                Console.WriteLine(i++);
+            }
+        }
+
+        // Extra, samma som punkt 1 och 2 ovan med en ”do { ... } while( <villkor> )”loopistället.
+        private static void EttTillTjugoExtra()
+        {
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine(i++);
+            } while (i <= 20);
+        }
+    }
+}
---
+++ Exercises/Håkan/Övning 4 – Loopar While/2_OneToHundred/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using bflibcs;
+
+namespace _2_OneToHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapa en Console Application där användaren får mata in ett tal mellan 1 och 100. Programmet skriver sedan ut alla tal 
+            // utifrån angivet tal upp till 100. Om tal utanför 1 till 100 matas in, avslutas programmet.
+            EttTillHundra();
+            //EttTillHundraExtra();
+        }
+
+        private static void EttTillHundra()
+        {
+            double num = ConsoleExt.GetNumber("Skriv in ett tal mellan 1 och 100: ");
+            if (num > 0 && num <= 100)
+            {
+                while (num < 101)
+                {
+                    Console.WriteLine(num++);
+                }
+            }
+        }
+
+        // Extra, samma som punkt 1 och 2 ovan med en ”do { ... } while( <villkor> )”loopistället.
+        private static void EttTillHundraExtra()
+        {
+            double num = ConsoleExt.GetNumber("Skriv in ett tal mellan 1 och 100: ");
+
+
+            if (num > 0 && num <= 100)
+            {
+                do
+                {
+                    Console.WriteLine(num++);
+                } while (num < 101);
+            }
+        }
+    }
+}
