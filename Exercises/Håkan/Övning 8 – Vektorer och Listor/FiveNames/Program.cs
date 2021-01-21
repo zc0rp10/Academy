f8763a1933f75f632e15af8925d087e75b945f1e
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-21 16:20:09 +0100

    Dag 4 - Övning 8

---
+++ Exercises/Håkan/Övning 8 – Vektorer och Listor/FiveNames/Program.cs
@@ -0,0 +1,45 @@
+﻿using bflibcs;
+using System;
+
+namespace FiveNames
+{
+    /* Skapa en Console Application:
+     * 1.   ..som skapar en string-vektor med fem element. Användaren får mata in fem namn via en for-loop.
+     *      Programmet skriver sedan ut namnen i en annan for-loop i omvänd ordning.
+     * 2.   ... lägg till funktionalitet till lösningen ovan för att byta ut ett namn på ett givet index.
+     * 3.   ... som tar in data från en väderstation. Programmet ska fråga hur många mätningar som har gjorts.
+     *      Programmet ska sedan fråga efter mätdata, och skriva ut medeltemperaturen.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+            GetNames(names);
+            Console.WriteLine();
+            PrintNames(names);
+
+            int index = ConsoleExt.GetInt("Enter a number to replace that name: ");
+            Console.Write("Enter a new name: ");
+            names[index -1] = Console.ReadLine();
+
+            PrintNames(names);
+        }
+
+        private static void PrintNames(string[] names)
+        {
+            for (int i = names.Length; i --> 0;)
+            {
+                Console.WriteLine($"{i+1}. {names[i]}");
+            }
+        }
+
+        private static void GetNames(string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Enter name nbr {i+1} of {names.Length}: ");
+                names[i] = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exercises/Håkan/Övning 8 – Vektorer och Listor/WeatherStation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using bflibcs;
+
+namespace WeatherStation
+{
+    /* 3.   Skapa en console app som tar in data från en väderstation.Programmet ska fråga hur många
+     *      mätningar som har gjorts. Programmet ska sedan fråga efter mätdata, och skriva ut medeltemperaturen.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbrOfM = ConsoleExt.GetInt("Enter the number of measurements taken: ");
+            int[] measurements = GetMeasurmentDetails(nbrOfM);
+
+            int sum = GetSum(measurements);
+            float average = (float)sum / nbrOfM;
+
+            Console.WriteLine($"The average temperature is: {average:f2} degrees.");
+
+        }
+
+        private static int GetSum(int[] measurements)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < measurements.Length; i++)
+            {
+                sum = sum + measurements[i];
+            }
+
+            return sum;
+        }
+
+        private static int[] GetMeasurmentDetails(int nbrOfM)
+        {
+            int[] measurements = new int[nbrOfM];
+            for (int i = 0; i < nbrOfM; i++)
+            {
+                measurements[i] = ConsoleExt.GetInt($"What was the result of measurment nbr {i + 1}: ");
+            }
+
+            return measurements;
+        }
+    }
+}
