3c888016de88730a0bb68bea2c0f6e050778d718
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-21 15:28:49 +0100

    Dag 4 - Övning MerryMethods

---
+++ Exercises/Håkan/MerryMethodsSolution/MerryMethodsProject/MethodBench.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MerryMethodsProject
+{
+    class MethodBench
+    {
+
+        public void FirstMethod()
+        {
+            Console.WriteLine("Marry Methods");
+        }
+
+        public void SecondMethod(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public void ThirdMethod(string str, bool value)
+        {
+            string input;
+
+            if (value == true)
+                input = str.ToUpper();
+            else
+                input = str.ToLower();
+
+            SecondMethod(input);
+        }
+
+        public bool FourthMethod()
+        {
+            ConsoleKeyInfo cki = Console.ReadKey(true);
+            bool isValid = false;
+
+            do
+            {
+                if (cki.Key == ConsoleKey.S)
+                {
+                    isValid = true;
+                    return true;
+                }
+                else if (cki.Key == ConsoleKey.W)
+                {
+                    isValid = true;
+                    return false;
+                }
+                else
+                    isValid = false;
+
+            } while (!isValid);
+            return false;
+        }
+
+        public Tuple<string, bool> SixthMethod()
+        {
+            string Name = Console.ReadLine();
+            bool LengthyName = (Name.Length > 5) ? true : false;
+
+            var tuple = new Tuple<string, bool>(Name, LengthyName);
+            
+            return tuple;
+        }
+    }
+}
---
+++ Exercises/Håkan/MerryMethodsSolution/MerryMethodsProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MerryMethodsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var methodBench = new MethodBench();
+
+            // First
+            int i = 1;
+            do
+            {
+                methodBench.FirstMethod();
+            } while (i++ < 3);
+
+            // Second
+            methodBench.SecondMethod("C# Now and Forever!");
+            Console.Write("Enter a string: ");
+            string userInput = Console.ReadLine();
+            methodBench.SecondMethod(userInput);
+
+            // Third
+            Console.Write("Print in uppercase (y/n)?: ");
+            bool userBool = (Console.ReadLine() == "y") ? true : false;
+            methodBench.ThirdMethod(userInput, userBool);
+
+            // Fourth
+            Console.Write("[W]hisper or [S]hout?: ");
+            bool shout = methodBench.FourthMethod();
+            methodBench.ThirdMethod(userInput, shout);
+
+            // Fifth(?)
+            methodBench.ThirdMethod(userInput, methodBench.FourthMethod());
+
+            // Sixth
+            Console.Write("Enter a name: ");
+            var myTuple = methodBench.SixthMethod();
+            if (myTuple.Item2 == true)
+                Console.WriteLine($"{myTuple.Item1} är ett långt namn.");
+            else
+                Console.WriteLine($"{myTuple.Item1} är inte ett långt namn.");
+        }
+    }
+}
