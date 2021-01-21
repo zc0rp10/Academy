1f9dbbe384a7c7f3fe568f18a3875472173b1dbd
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-01-21 22:50:21 +0100

    GameSounds Test

---
+++ Exercises/GameSounds/GameSounds/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace GameSounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer fxPlayer = new SoundPlayer();
+            PlayAttack(fxPlayer);
+            PlayFire(fxPlayer);
+            PlayIce(fxPlayer);
+            PlayNoMana(fxPlayer);
+            PlayVictory(fxPlayer);
+
+
+        }
+
+        static void PlayAttack(SoundPlayer fxPlayer)
+        {
+            string filePath = Directory.GetCurrentDirectory() + @"\sounds\attack.wav";
+            fxPlayer.SoundLocation = filePath;
+            fxPlayer.PlaySync();
+        }
+
+        static void PlayFire(SoundPlayer fxPlayer)
+        {
+            string filePath = Directory.GetCurrentDirectory() + @"\sounds\fire.wav";
+            fxPlayer.SoundLocation = filePath;
+            fxPlayer.PlaySync();
+        }
+
+        static void PlayIce(SoundPlayer fxPlayer)
+        {
+            string filePath = Directory.GetCurrentDirectory() + @"\sounds\ice.wav";
+            fxPlayer.SoundLocation = filePath;
+            fxPlayer.PlaySync();
+        }
+
+        static void PlayNoMana(SoundPlayer fxPlayer)
+        {
+            string filePath = Directory.GetCurrentDirectory() + @"\sounds\nomana.wav";
+            fxPlayer.SoundLocation = filePath;
+            fxPlayer.PlaySync();
+        }
+
+        static void PlayVictory(SoundPlayer fxPlayer)
+        {
+            string filePath = Directory.GetCurrentDirectory() + @"\sounds\victory.wav";
+            fxPlayer.SoundLocation = filePath;
+            fxPlayer.PlaySync();
+        }
+
+    }
+}
