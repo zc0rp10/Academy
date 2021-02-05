0b1a64cbc30b3d822051b449d1ddda104009aee4
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-02-05 09:48:38 +0100

    Assignment1 - first

---
+++ Assignments/Assessment 1/AnimalLibrary/Hamster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalLibrary
+{
+    public class Hamster
+    {
+        public Hamster(string name, bool isPet, HamsterSpecie specie)
+        {
+            Name = name;
+            IsPet = isPet;
+            Specie = specie;
+        }
+
+        public string Name { get; set; }
+        public bool IsPet { get; set; }
+        public HamsterSpecie Specie { get; set; }
+
+        public string Greet()
+        {
+            return $"{Name} is a {(IsPet == true ? "pet" : "wild")} {Specie} hamster.";
+        }
+    }
+}
---
+++ Assignments/Assessment 1/AnimalLibrary/MethodsCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace AnimalLibrary
+{
+    public class MethodsCollection
+    {
+        public static int AddThree(int n)
+        {
+            return n + 3;
+        }
+
+        public static int AddFourAndThenMultiplyByThree(int n)
+        {
+            return (n + 4) * 3;
+        }
+
+        public static string MaybeUppercaseAndDecorateText(string s, bool uppercase, bool decorate)
+        {
+            if (uppercase)
+               s = s.ToUpper();
+
+            if (decorate)
+            {
+                var sb = new StringBuilder(s);
+                sb.Insert(0,"-:");
+                sb.Append(":-");
+                s = sb.ToString();
+            }
+
+            return s;
+        }
+
+        public static string TellRelationToHundred(int number)
+        {
+            if (number < 0)
+                return $"{number} is a negative number";
+            else if (number > 100)
+                return $"{number} is greater than 100";
+            else
+                return $"Add {100-number} to {number} and you get 100";
+        }
+    }
+}
