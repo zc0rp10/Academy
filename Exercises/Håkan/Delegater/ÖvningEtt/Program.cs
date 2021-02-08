2075cce7bb15d486a44dc7ed82c16a7ee21b2f99
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-02-08 11:22:36 +0100

    Delegater - Övning 1

---
+++ Exercises/Håkan/Delegater/ÖvningEtt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Delegater01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action carAction;
+            carAction = Accelerate;
+            carAction();
+            carAction = Decelerate;
+            carAction();
+            
+        }
+
+        static void Accelerate()
+        {
+            Console.WriteLine("Accelerating");
+        }
+
+        static void Decelerate()
+        {
+            Console.WriteLine("Decelerating");
+        }
+    }
+}
