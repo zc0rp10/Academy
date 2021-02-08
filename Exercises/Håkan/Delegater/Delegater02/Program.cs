6d15c5efe76823f9d9eb468741549937745f7ddd
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-02-08 13:04:39 +0100

    Delegarer - Övning 2

---
+++ Exercises/Håkan/Delegater/Delegater02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegater02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string, bool> p = PrintIt;
+            p("My string to print", false);
+            p("My string to print", true);
+        }
+
+        static void PrintIt(string value, bool toUpper)
+        {
+            if (toUpper)
+                value = value.ToUpper();
+
+            Console.WriteLine(value);
+        }
+    }
+}
