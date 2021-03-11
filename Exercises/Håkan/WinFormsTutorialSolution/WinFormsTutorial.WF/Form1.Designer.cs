af56350760081b670323402dd09914f42a66dcfe
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-03-11 18:12:52 +0100

    Håkan - WinForms Genomgång

---
+++ Exercises/Håkan/WinFormsTutorialSolution/WinFormsTutorial.WF/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace WinFormsTutorial.WF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.lstNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSayHello
+            // 
+            this.btnSayHello.Location = new System.Drawing.Point(356, 37);
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.Size = new System.Drawing.Size(75, 23);
+            this.btnSayHello.TabIndex = 0;
+            this.btnSayHello.Text = "Press Me...";
+            this.btnSayHello.UseVisualStyleBackColor = true;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(5, 8);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(426, 23);
+            this.textBoxUserName.TabIndex = 1;
+            this.textBoxUserName.Text = "Type your name then..";
+            // 
+            // lstNames
+            // 
+            this.lstNames.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.ItemHeight = 15;
+            this.lstNames.Location = new System.Drawing.Point(5, 305);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(426, 94);
+            this.lstNames.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 404);
+            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.btnSayHello);
+            this.Name = "MainForm";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "My Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSayHello;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.ListBox lstNames;
+    }
+}
+
---
+++ Exercises/Håkan/WinFormsTutorialSolution/WinFormsTutorial.WF/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTutorial.WF
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSayHello_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxUserName.Text))
+            {
+                lstNames.Items.Add(textBoxUserName.Text);
+                textBoxUserName.Text = "";
+            }
+        }
+    }
+}
