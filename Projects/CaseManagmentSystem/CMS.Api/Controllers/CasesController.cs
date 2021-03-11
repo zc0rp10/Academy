df7b2031e6e4bb56114dc3f08cfc736c836c0082
Björn <55206983+zc0rp10@users.noreply.github.com>
2021-03-11 18:13:40 +0100

    Case Managment Project

---
+++ Projects/CaseManagmentSystem/CMS.Api/Controllers/CasesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CMS.Api.Models;
+
+namespace CMS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CasesController : ControllerBase
+    {
+        private readonly CaseContext _context;
+
+        public CasesController(CaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cases
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Case>>> GetCases()
+        {
+            return await _context.Cases.ToListAsync();
+        }
+
+        // GET: api/Cases/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Case>> GetCase(long id)
+        {
+            var @case = await _context.Cases.FindAsync(id);
+
+            if (@case == null)
+            {
+                return NotFound();
+            }
+
+            return @case;
+        }
+
+        // PUT: api/Cases/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCase(long id, Case @case)
+        {
+            if (id != @case.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@case).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CaseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cases
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Case>> PostCase(Case @case)
+        {
+            _context.Cases.Add(@case);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCase), new { id = @case.Id }, @case);
+        }
+
+        // DELETE: api/Cases/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCase(long id)
+        {
+            var @case = await _context.Cases.FindAsync(id);
+            if (@case == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cases.Remove(@case);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CaseExists(long id)
+        {
+            return _context.Cases.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Projects/CaseManagmentSystem/CMS.Api/Models/CaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Api.Models
+{
+    public class CaseContext : DbContext
+    {
+        public CaseContext(DbContextOptions<CaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Case> Cases { get; set; }
+    }
+}
